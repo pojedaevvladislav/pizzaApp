732e8cb04f25321f4e760387f8c3566bfd91f31a
Vladislav Pojedaev <pojedaev.vladislav@gmail.com>
2025-10-27 16:38:02 +0200

    Создал счётчик и лэйблы

--- src/app/data/services/pizza.ts
+++ src/app/data/services/pizza.ts
@@ -1,5 +1,10 @@
 import {PizzaAppInterface} from "../interface/pizza-app.interface";
 import {of} from "rxjs";
+import {Injectable} from "@angular/core";
+
+@Injectable({
+  providedIn: 'root'
+})
 
 export class PizzaService {
   private pizzaList: PizzaAppInterface[];
@@ -12,6 +17,24 @@ export class PizzaService {
       name: 'Broccolo',
       ingredients: 'sos pentru pizza, vinete coapte, dovlecei, ciuperci champinion, usturoi, tofu, frișca vegetală',
       isVegetarian: true,
+      isSpicy: false,
+      price: 124,
+      weight: 680
+    },{
+      pizzaId: 2,
+      imageFileName: 'broccolo-1.png',
+      name: 'Broccolo',
+      ingredients: 'sos pentru pizza, vinete coapte, dovlecei, ciuperci champinion, usturoi, tofu, frișca vegetală',
+      isVegetarian: false,
+      isSpicy: true,
+      price: 124,
+      weight: 680
+    },{
+      pizzaId: 3,
+      imageFileName: 'broccolo-1.png',
+      name: 'Broccolo',
+      ingredients: 'sos pentru pizza, vinete coapte, dovlecei, ciuperci champinion, usturoi, tofu, frișca vegetală',
+      isVegetarian: false,
       isSpicy: false,
       price: 124,
       weight: 680
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,26 +2,40 @@ import {Component, inject, Input, OnInit} from '@angular/core';
 import {IonHeader, IonToolbar, IonTitle, IonContent, IonIcon} from '@ionic/angular/standalone';
 import {PizzaService} from "../data/services/pizza";
 import {PizzaAppInterface} from "../data/interface/pizza-app.interface";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, CommonModule],
 })
-export class HomePage{
+export class HomePage implements OnInit{
 
-  // pizzas: PizzaAppInterface[] = [];
-  //
-  // private pizzaService = inject(PizzaService);
-  //
-  // constructor() {}
-  //
-  // @Input() pizza?: PizzaAppInterface;
-  //
-  // ngOnInit() {
-  //   this.pizzaService.getPizzas().subscribe(pizzas => {
-  //     this.pizzas = pizzas;
-  //   })
-  // }
+  pizzas: PizzaAppInterface[] = [];
+
+  private pizzaService = inject(PizzaService);
+
+  cartCount: { [pizzaId: number]: number } = {};
+
+  constructor() {}
+
+  @Input() pizza?: PizzaAppInterface;
+
+  ngOnInit() {
+    this.pizzaService.getPizzas().subscribe(pizzas => {
+      this.pizzas = pizzas;
+      pizzas.forEach(pizza => this.cartCount[pizza.pizzaId] = 0);
+    });
+  }
+
+  addToCart(pizzaId: number): void {
+    this.cartCount[pizzaId]++;
+  }
+
+  decrease(pizzaId: number): void {
+    if (this.cartCount[pizzaId] > 0) this.cartCount[pizzaId]--;
+  }
+
 }
