449f94603754a04ae0b1ddc38a3e1d279abe1f6a
Vladislav Pojedaev <pojedaev.vladislav@gmail.com>
2025-10-28 14:25:19 +0200

    Создал нав-бар и стилизовал его

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {IonHeader} from "@ionic/angular/standalone";
 
 @Component({
-  selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss'],
+    selector: 'app-nav-bar',
+    templateUrl: './nav-bar.component.html',
+    styleUrls: ['./nav-bar.component.scss'],
+    imports: [
+        IonHeader
+    ]
 })
 export class NavBarComponent  implements OnInit {
 
